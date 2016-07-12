e01a43d14bbd4bb04df3e3a6d3002344185c83aa
Orlov Sergey <s.orlov@rambler-co.ru>
2016-07-12 13:43:59 +0300

    babel

--- main-compiled.js
+++ main-compiled.js
@@ -1,23 +1,21 @@
-var doElsCollide = function(el1, el2) {
+"use strict";
+
+var doElsCollide = function doElsCollide(el1, el2) {
 
   var rect1 = el1.getBoundingClientRect();
   var rect2 = el2.getBoundingClientRect();
 
-  return (rect1.left < rect2.left + rect2.width &&
-    rect1.left + rect1.width > rect2.left &&
-    rect1.top < rect2.top + rect2.height &&
-    rect1.height + rect1.top > rect2.top);
+  return rect1.left < rect2.left + rect2.width && rect1.left + rect1.width > rect2.left && rect1.top < rect2.top + rect2.height && rect1.height + rect1.top > rect2.top;
 };
 
-var getColor = function(percent) {
+var getColor = function getColor(percent) {
   if (percent > 1) return "rgb(180,32,37)";
 
   var r = Math.floor(232 - (232 - 180) * percent);
   var g = Math.floor(232 - (232 - 32) * percent);
   var b = Math.floor(232 - (232 - 37) * percent);
 
-  return `rgb(${r},${g},${b})`;
-
+  return "rgb(" + r + "," + g + "," + b + ")";
 };
 
 function insertAfter(elem, refElem) {
@@ -26,21 +24,20 @@ function insertAfter(elem, refElem) {
 
 // ------------Data process functions------
 
-
-let newDataProseed = function(csvFile) {
+var newDataProseed = function newDataProseed(csvFile) {
   var r = {};
   var regionsArr = csvFile.split("\n");
   regionsArr.pop(); //remove end line
 
-  regionsArr.forEach(function(e, i) {
+  regionsArr.forEach(function (e, i) {
     e = e.split(";");
-    let key = e.shift();
-    let shortName = e.shift();
-    let name = e.shift();
+    var key = e.shift();
+    var shortName = e.shift();
+    var name = e.shift();
     r[key] = {
       name: name,
       shortName: shortName,
-      rowYearsData: e,
+      rowYearsData: e
     };
   });
 
@@ -49,55 +46,43 @@ let newDataProseed = function(csvFile) {
     years.push(1994 + i);
   }
 
-
-
-  Object.keys(r).forEach(function(region) {
+  Object.keys(r).forEach(function (region) {
 
     r[region].absDied = {};
     r[region].absInfected = {};
     r[region].relInfected = {};
-    years.forEach(function(year) {
+    years.forEach(function (year) {
       r[region].absInfected[year] = infectedInYear(year, r[region].rowYearsData);
       r[region].absDied[year] = diedInYear(year, r[region].rowYearsData);
       r[region].relInfected[year] = relnIfectedInYear(year, r[region].rowYearsData);
     });
-
-
   });
 
   return r;
-
 };
 
-let infectedInYear = function(year, rowRregData) {
-  let ofset = 0 + (2014 - year) * 3;
+var infectedInYear = function infectedInYear(year, rowRregData) {
+  var ofset = 0 + (2014 - year) * 3;
   return rowRregData[ofset];
 };
 
-
-let diedInYear = function(year, rowRregData) {
-  let ofset = 1 + (2014 - year) * 3;
+var diedInYear = function diedInYear(year, rowRregData) {
+  var ofset = 1 + (2014 - year) * 3;
   return rowRregData[ofset];
 };
 
-let relnIfectedInYear = function(year, rowRregData) {
-  let ofset = 2 + (2014 - year) * 3;
+var relnIfectedInYear = function relnIfectedInYear(year, rowRregData) {
+  var ofset = 2 + (2014 - year) * 3;
   return rowRregData[ofset];
 };
 
-
-
-
-
-$(function() {
+$(function () {
 
   //----Data----
-  let data = {
-
-  };
+  var data = {};
 
   // -------------Global State------------
-  let state = {
+  var state = {
     year: 1999,
     regionId: "",
     display: "abs"
@@ -111,29 +96,28 @@ $(function() {
   ██      ██ ██   ██ ██
   */
 
-  var map = (function() {
-
+  var map = function () {
 
     var map = {
       render: null,
       selectedReg: null,
-      mapElem: null,
+      mapElem: null
     };
     map.mapElem = document.getElementById("svg-map");
     var regions = $("#svg-map path, #svg-map polygon");
     var btn = $(".map .map_header .btn");
     var selectedReg = null;
 
-    btn.click(function(e) {
+    btn.click(function (e) {
       e.stopPropagation();
       // btn.toggleClass("active");
       state.display = this.dataset.displaytype;
       renderAll();
     });
 
-    var setRegsColor = function(year) {
+    var setRegsColor = function setRegsColor(year) {
 
-      Object.keys(data).forEach(function(reginoId) {
+      Object.keys(data).forEach(function (reginoId) {
 
         var value, percent;
 
@@ -144,7 +128,7 @@ $(function() {
           if (value < 100) {
             percent = 0;
           } else {
-            percent = (Math.log2(value / 100)) / 9;
+            percent = Math.log2(value / 100) / 9;
           }
         } else {
           value = data[reginoId].relInfected[year];
@@ -152,18 +136,17 @@ $(function() {
           if (value < 10) {
             percent = 0;
           } else {
-            percent = (Math.log2(value / 10)) / 9;
+            percent = Math.log2(value / 10) / 9;
           }
-
         }
 
         $('#' + reginoId).css({
-          'fill': getColor(percent),
+          'fill': getColor(percent)
         });
       });
     };
 
-    var setSelectedRegion = function(regionId) {
+    var setSelectedRegion = function setSelectedRegion(regionId) {
       map.selectedReg && map.selectedReg.classList.remove('selected');
       if (regionId) {
         map.selectedReg = document.getElementById(regionId);
@@ -171,14 +154,13 @@ $(function() {
       }
     };
 
-    var setButtons = function(display){
-      btn.each(function(i,elem){
-        if(elem.dataset.displaytype == display) elem.classList.add('active');
-        else elem.classList.remove('active');
+    var setButtons = function setButtons(display) {
+      btn.each(function (i, elem) {
+        if (elem.dataset.displaytype == display) elem.classList.add('active');else elem.classList.remove('active');
       });
     };
 
-    var render = function() {
+    var render = function render() {
       setRegsColor(state.year);
       setSelectedRegion(state.regionId);
       setButtons(state.display);
@@ -189,26 +171,23 @@ $(function() {
       }
     };
 
-    regions.click(
-      function(e) {
-        e.stopPropagation();
-        if (e.target.id === state.regionId) {
-          map.mapElem.classList.remove('regSelected');
-          state.regionId = "";
-        } else {
-          state.regionId = e.target.id;
-          e.target.parentElement.insertBefore(e.target, null);
-        }
-        renderAll();
+    regions.click(function (e) {
+      e.stopPropagation();
+      if (e.target.id === state.regionId) {
+        map.mapElem.classList.remove('regSelected');
+        state.regionId = "";
+      } else {
+        state.regionId = e.target.id;
+        e.target.parentElement.insertBefore(e.target, null);
       }
-    );
+      renderAll();
+    });
 
     map.render = render;
     map.selectedReg = selectedReg;
 
     return map;
-
-  })();
+  }();
 
   /*
   ██      ███████  ██████  ███████ ███    ██ ██████
@@ -218,13 +197,12 @@ $(function() {
   ███████ ███████  ██████  ███████ ██   ████ ██████
   */
 
-
-  var legend = (function() {
+  var legend = function () {
 
     //----Init legned ------
 
-    var initColors = function() {
-      $(".legend .bloc .color").each(function(id, e) {
+    var initColors = function initColors() {
+      $(".legend .bloc .color").each(function (id, e) {
         var color = getColor((id + 1) / 10);
         $(e).css({
           "background-color": color
@@ -232,28 +210,28 @@ $(function() {
       });
     };
 
-    var renderValues = function() {
+    var renderValues = function renderValues() {
       var multiplier = state.display == "abs" ? 100 : 10;
 
-      $(".legend .bloc .val").each(function(id, e) {
+      $(".legend .bloc .val").each(function (id, e) {
         $(e).text(multiplier * Math.pow(2, id));
       });
     };
 
-    var init = function() {
+    var init = function init() {
       initColors();
       renderValues();
     };
 
-    var render = function() {
+    var render = function render() {
       renderValues();
     };
 
     return {
       "init": init,
-      render: render,
+      render: render
     };
-  })();
+  }();
 
   /*
   ██    ██ ███████  █████  ██████  ███████
@@ -263,11 +241,11 @@ $(function() {
      ██    ███████ ██   ██ ██   ██ ███████
   */
 
-  var years = (function() {
+  var years = function () {
 
-    var render = function() {
+    var render = function render() {
 
-      $(".years .col").each(function(id, e) {
+      $(".years .col").each(function (id, e) {
         var year = parseInt($(e).attr("id"));
         if (year === state.year) {
           $(e).addClass("active");
@@ -275,12 +253,11 @@ $(function() {
           $(e).removeClass("active");
         }
       });
-
     };
 
     // _____________click__________
 
-    $(".years .col").on("click", function(e) {
+    $(".years .col").on("click", function (e) {
       e.stopPropagation();
       var year = parseInt($(this).attr("id"));
       state.year = year;
@@ -290,9 +267,7 @@ $(function() {
     return {
       render: render
     };
-
-
-  })();
+  }();
 
   /*
   ██████  ██████   ██████  ██████      ██████   ██████  ██     ██ ███    ██
@@ -302,7 +277,7 @@ $(function() {
   ██████  ██   ██  ██████  ██          ██████   ██████   ███ ███  ██   ████
   */
 
-  var dropDowdn = (function() {
+  var dropDowdn = function () {
 
     var isOpen = false;
     var $select = $(".map_header .drop_down .head");
@@ -311,31 +286,28 @@ $(function() {
     var container = $(".scrollable .content");
     var head = $(".drop_down .text");
 
-    $select.click(
-      function(e) {
-        e.stopPropagation();
-        if (isOpen) {
-          close();
-        } else {
-          open();
-        }
+    $select.click(function (e) {
+      e.stopPropagation();
+      if (isOpen) {
+        close();
+      } else {
+        open();
       }
-    );
+    });
 
-
-    var close = function() {
+    var close = function close() {
       scrollable.css('visibility', 'hidden');
       isOpen = false;
       closeImg[0].style.transform = "rotate(0deg)";
     };
 
-    var open = function() {
+    var open = function open() {
       scrollable.css('visibility', 'visible');
       isOpen = true;
       closeImg[0].style.transform = "rotate(180deg)";
     };
 
-    var render = function() {
+    var render = function render() {
       if (state.regionId) {
         head.text(data[state.regionId].shortName);
       } else {
@@ -345,41 +317,38 @@ $(function() {
       // state.regionId && head.text(data[state.regionId].shortName || "Регион")
       // Dirty Hack
       container.empty();
-      Object.keys(data).forEach(
-        function(region) {
+      Object.keys(data).forEach(function (region) {
 
-          let shortName = data[region].shortName;
+        var shortName = data[region].shortName;
 
-          var elem = $(`<div class="item" data-regionId="${region}"> ${shortName} </div>`);
+        var elem = $("<div class=\"item\" data-regionId=\"" + region + "\"> " + shortName + " </div>");
 
-          if (region === state.regionId) {
-            elem = $(`<div class="active" data-regionId="${region}"> ${shortName} </div>`);
-          }
-
-          container.append(elem);
-
-          elem.click(function(e) {
-            e.stopPropagation();
-            state.regionId = this.dataset.regionid;
-            renderAll();
-            close();
-          });
+        if (region === state.regionId) {
+          elem = $("<div class=\"active\" data-regionId=\"" + region + "\"> " + shortName + " </div>");
         }
-      );
+
+        container.append(elem);
+
+        elem.click(function (e) {
+          e.stopPropagation();
+          state.regionId = this.dataset.regionid;
+          renderAll();
+          close();
+        });
+      });
       close();
     };
 
-
-    var createScroller = (function() {
+    var createScroller = function () {
 
       var scrollContainer = document.querySelector('.scrollable'),
-        scrollContentWrapper = document.querySelector('.scrollable .content-wrapper'),
-        scrollContent = document.querySelector('.scrollable .content'),
-        contentPosition = 0,
-        scrollerBeingDragged = false,
-        scroller,
-        topPosition,
-        scrollerHeight;
+          scrollContentWrapper = document.querySelector('.scrollable .content-wrapper'),
+          scrollContent = document.querySelector('.scrollable .content'),
+          contentPosition = 0,
+          scrollerBeingDragged = false,
+          scroller,
+          topPosition,
+          scrollerHeight;
 
       function calculateScrollerHeight() {
         // *Calculation of how tall scroller should be
@@ -437,25 +406,21 @@ $(function() {
           window.addEventListener('mouseup', stopDrag);
           window.addEventListener('mousemove', scrollBarScroll);
         }
-
       }
 
       // *** Listeners ***
       scrollContentWrapper.addEventListener('scroll', moveScroller);
 
       return createScroller;
-
-    }());
-
+    }();
 
     return {
       open: open,
       close: close,
       render: render,
-      createScroller: createScroller,
+      createScroller: createScroller
     };
-
-  })();
+  }();
 
   /*
   ██████   ██████   ██████ ██    ██ ███    ███ ███████ ███    ██ ████████      ██████ ██      ██  ██████ ██   ██ ███████
@@ -465,12 +430,10 @@ $(function() {
   ██████   ██████   ██████  ██████  ██      ██ ███████ ██   ████    ██         ██████ ███████ ██  ██████ ██   ██ ███████
   */
 
-  document.body.addEventListener("click",
-    function(e) {
-      state.regionId = "";
-      renderAll();
-    }
-  );
+  document.body.addEventListener("click", function (e) {
+    state.regionId = "";
+    renderAll();
+  });
 
   /*
   ██████   ██████  ██████  ██    ██ ██████
@@ -480,8 +443,7 @@ $(function() {
   ██       ██████  ██       ██████  ██
   */
 
-
-  var popUp = (function() {
+  var popUp = function () {
 
     var popUp = $(".banner");
     var closeButton = $(".banner .head .btn.close");
@@ -493,35 +455,34 @@ $(function() {
     var diedFeald = dataFields.find(".dead");
     var infectedTextFeald = $(dataFields.find(".leble")[0]);
 
-    var close = function() {
+    var close = function close() {
       hide();
       state.regionId = "";
       renderAll();
     };
 
-    var hide = function() {
+    var hide = function hide() {
       popUp.css('opacity', 0);
       popUp.css('visibility', "hidden");
-
     };
 
-    var open = function() {
+    var open = function open() {
       popUp.css('opacity', 1);
       popUp.css('visibility', "visible");
     };
-    popUp.click(function(e) {
+    popUp.click(function (e) {
       e.stopPropagation();
     });
 
-    closeButton.click(function(e) {
+    closeButton.click(function (e) {
       close();
     });
 
-    var renderPie = (function() {
+    var renderPie = function () {
       var path = null;
       var svgElem = document.getElementById("svg-pie");
 
-      var renderPie = function(deg) {
+      var renderPie = function renderPie(deg) {
         if (!svgElem) return;
         if (path) {
           svgElem.removeChild(path);
@@ -531,25 +492,21 @@ $(function() {
 
         if (deg > 359) {
           path = svgElem.querySelector("circle").cloneNode(true);
-          path.setAttribute("fill", "url(#img1)" );
+          path.setAttribute("fill", "url(#img1)");
           svgElem.appendChild(path);
           return;
         }
 
-
-
         var cx = 40,
-          cy = 40,
-          rx = 40,
-          ry = 40;
+            cy = 40,
+            rx = 40,
+            ry = 40;
 
         var p = svgElem.createSVGPoint();
         p.x = 0;
         p.y = 1;
 
-
         var m = svgElem.createSVGMatrix();
-
 
         var p2 = p.matrixTransform(m.rotate(deg));
 
@@ -557,7 +514,6 @@ $(function() {
         p2.y = cy - p2.y * ry;
 
         path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-
 
         if (deg > 180) {
           var d = "M" + cx + " " + (cy - ry) + "A" + rx + " " + ry + " 0 1 1" + p2.x + " " + p2.y + "L" + cx + " " + cy + "z";
@@ -569,13 +525,11 @@ $(function() {
         path.setAttribute("fill", "url(#img1)");
 
         svgElem.appendChild(path);
-
       };
       return renderPie;
-    })();
+    }();
 
-
-    var findPosition = function() {
+    var findPosition = function findPosition() {
 
       if (!map.selectedReg) return;
 
@@ -587,33 +541,31 @@ $(function() {
 
       var left = regRect.left + regRect.width;
       var top = regRect.top - popUpRect.height;
-      if(top < mapRect.top) {
+      if (top < mapRect.top) {
         top = mapRect.top + 20;
       }
-      if(left + popUpRect.width > mapRect.left + mapRect.width) {
-         left = regRect.left - popUpRect.width;
-       }
+      if (left + popUpRect.width > mapRect.left + mapRect.width) {
+        left = regRect.left - popUpRect.width;
+      }
 
-       left = left + pageXOffset;
-       top = top + pageYOffset;
+      left = left + pageXOffset;
+      top = top + pageYOffset;
 
       return {
-        top: top ,
-        left: left,
+        top: top,
+        left: left
       };
     };
 
-    var setPosition = function(obj) {
-      if(!obj) return;
+    var setPosition = function setPosition(obj) {
+      if (!obj) return;
       var format = ["right", "top", "left", "bottom"];
-      format.forEach(function(prop) {
+      format.forEach(function (prop) {
         popUp[0].style[prop] = obj[prop] ? obj[prop] + "px" : "";
       });
     };
 
-
-
-    var render = function() {
+    var render = function render() {
 
       if (!state.regionId) {
         hide();
@@ -656,16 +608,15 @@ $(function() {
       }
     };
 
-    var onresize = function(){
+    var onresize = function onresize() {
       setPosition(findPosition());
     };
 
     return {
       render: render,
-      onresize: onresize,
+      onresize: onresize
     };
-
-  })();
+  }();
 
   /*
   ██████  ███████ ███    ██ ██████  ███████ ██████
@@ -675,7 +626,7 @@ $(function() {
   ██   ██ ███████ ██   ████ ██████  ███████ ██   ██
   */
 
-  var renderAll = function() {
+  var renderAll = function renderAll() {
     map.render();
     years.render();
     dropDowdn.render();
@@ -683,15 +634,14 @@ $(function() {
     popUp.render();
   };
 
-  var initAll = function() {
+  var initAll = function initAll() {
     legend.init();
     dropDowdn.createScroller();
   };
 
-  window.onresize = function(){
+  window.onresize = function () {
     popUp.onresize();
   };
-
 
   /*
   ██████  ███████  █████  ██████      ██████   █████  ████████  █████
@@ -701,25 +651,23 @@ $(function() {
   ██   ██ ███████ ██   ██ ██████      ██████  ██   ██    ██    ██   ██
   */
 
-  (function() {
+  (function () {
     var blob = null;
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "HIV_Data_by_reg.csv");
     xhr.responseType = "blob"; //force the HTTP response, response-type header to be blob
-    xhr.onload = function() {
+    xhr.onload = function () {
       blob = xhr.response; //xhr.response is now a blob object
       myReader.readAsText(blob);
     };
     xhr.send();
 
     var myReader = new FileReader();
-    myReader.addEventListener("loadend", function(e) {
+    myReader.addEventListener("loadend", function (e) {
 
       data = newDataProseed(e.srcElement.result);
       initAll();
       renderAll();
-
     });
   })();
-
 });
